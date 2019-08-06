2a4265a47ed2d4e701fed0bd420d4cfd41b0f051
Oliver Caldwell <ollie@oli.me.uk>
2019-08-06 18:33:17 +0100

    Kill util, move rebel-readline stuff into core

--- src/propel/core.clj
+++ src/propel/core.clj
@@ -1,12 +1,39 @@
 (ns propel.core
   "Tools to start prepl servers in various configurations."
   (:require [clojure.core.server :as server]
-            [propel.util :as util]))
+            [clojure.main :as clojure]
+            [rebel-readline.core :as rebel]
+            [rebel-readline.clojure.main :as rebel-clojure]
+            [rebel-readline.clojure.line-reader :as rebel-line-reader]
+            [rebel-readline.clojure.service.local :as rebel-local-service])
+  (:import [java.net ServerSocket]))
 
 ;; TODO Tidy up how defaults work.
 ;; TODO Wrap functions in spec with expound printing.
 
-(defn prepl
+(defn- free-port
+  "Find a free port we can bind to."
+  []
+  (let [socket (ServerSocket. 0)]
+    (.close socket)
+    (.getLocalPort socket)))
+
+(defn- noop
+  "A function that does nothing."
+  []
+  nil)
+
+(defn start-rebel-readline
+  "Start a rebel-readline REPL."
+  []
+  (rebel/with-line-reader
+    (rebel-line-reader/create
+      (rebel-local-service/create))
+    (clojure/repl
+      :prompt noop
+      :read (rebel-clojure/create-repl-read))))
+
+(defn start-prepl
   "Start a prepl server."
   [{:keys [port address env port-file? port-file-name] :as opts}]
   (let [env (or env :jvm)
@@ -21,7 +48,7 @@
                                :graaljs 'cljs.server.graaljs/prepl
                                :nashorn 'cljs.server.nashorn/prepl)
                      :name (str (gensym "propel-server-"))
-                     :port (or port (util/free-port))
+                     :port (or port (free-port))
                      :address (or address "127.0.0.1")
                      :port-file-name port-file-name})]
 
--- src/propel/main.clj
+++ src/propel/main.clj
@@ -1,17 +1,12 @@
 (ns propel.main
   "CLI for starting prepl servers and dropping into a REPL."
-  (:require [clojure.main :as clojure]
-            [rebel-readline.core :as rebel]
-            [rebel-readline.clojure.main :as rebel-clojure]
-            [rebel-readline.clojure.line-reader :as rebel-line-reader]
-            [rebel-readline.clojure.service.local :as rebel-local-service]
-            [propel.core :as propel]
-            [propel.util :as util]))
+  (:require [propel.core :as propel]))
 
 ;; TODO Add CLI argument parsing.
-;; TODO Multiple prepls in one go?
 
-(defn -main []
+(defn -main
+  "Allows you to easily start a single prepl then drop into a rebel-readline REPL."
+  []
   (println "Clojure" (clojure-version))
 
   (let [{:keys [address env port port-file? port-file-name]}
@@ -22,9 +17,4 @@
     (when port-file?
       (println "Port written to" port-file-name)))
 
-  (rebel/with-line-reader
-    (rebel-line-reader/create
-      (rebel-local-service/create))
-    (clojure/repl
-      :prompt util/noop
-      :read (rebel-clojure/create-repl-read))))
+  (propel/start-rebel-readline))
